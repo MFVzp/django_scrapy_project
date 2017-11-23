ead1f1f3410fb9e916327896cbf7757c2ea363ed
Ilya <tauruszpua@yahoo.com>
2017-11-23 16:50:04 +0200

    change pipelines

--- parser_project/scrapy_project/scrapy_project/pipelines.py
+++ parser_project/scrapy_project/scrapy_project/pipelines.py
@@ -8,13 +8,19 @@ class ScTestPipeline(object):
     def __init__(self):
         self.store = list()
 
+    def save_items(self):
+        for store_item in self.store:
+            if isinstance(store_item, ProductItem):
+                add_product_item.delay(dict(store_item))
+            elif isinstance(store_item, PriceItem):
+                add_price_item.delay(dict(store_item))
+        self.store.clear()
+
     def process_item(self, item, spider):
         self.store.append(item)
-        if len(self.store) > 5:
-            for store_item in self.store:
-                if isinstance(store_item, ProductItem):
-                    add_product_item.delay(dict(store_item))
-                elif isinstance(store_item, PriceItem):
-                    add_price_item.delay(dict(store_item))
-            self.store.clear()
+        if len(self.store) > 100:
+            self.save_items()
         return item
+
+    def close_spider(self, spider):
+        self.store and self.save_items()
