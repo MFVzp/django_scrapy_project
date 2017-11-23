aea3b1b4d3632694ede7cf5d7af8c4639ca409d3
Ilya <tauruszpua@yahoo.com>
2017-11-23 16:39:52 +0200

    add piplines

--- parser_project/scrapy_project/scrapy_project/pipelines.py
+++ parser_project/scrapy_project/scrapy_project/pipelines.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from .tasks import add_product_item, add_price_item
+from .items import ProductItem, PriceItem
+from .settings import NUMBER_OF_ITEMS_TO_INSERT
 
 
-class ScrapyProjectPipeline(object):
+class ScTestPipeline(object):
+
+    def __init__(self):
+        self.store = list()
+
     def process_item(self, item, spider):
+        self.store.append(item)
+        if len(self.store) > NUMBER_OF_ITEMS_TO_INSERT:
+            for store_item in self.store:
+                if isinstance(store_item, ProductItem):
+                    add_product_item.delay(dict(store_item))
+                elif isinstance(store_item, PriceItem):
+                    add_price_item.delay(dict(store_item))
+            self.store.clear()
         return item
