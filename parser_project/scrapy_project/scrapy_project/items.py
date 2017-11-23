5fc06678bd59b58ca12964895a499816d37c89fc
Ilya <tauruszpua@yahoo.com>
2017-11-23 16:37:24 +0200

    add items

--- parser_project/scrapy_project/scrapy_project/items.py
+++ parser_project/scrapy_project/scrapy_project/items.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class ScrapyProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ProductItem(scrapy.Item):
+    site_product_id = scrapy.Field()
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    categories = scrapy.Field()
+    description = scrapy.Field()
+    material = scrapy.Field()
+    made_in = scrapy.Field()
+    url = scrapy.Field()
+    images = scrapy.Field()
+    site = scrapy.Field()
+
+
+class PriceItem(scrapy.Item):
+    site_product_id = scrapy.Field()
+    params = scrapy.Field()
+    stock_level = scrapy.Field()
+    currency = scrapy.Field()
+    date = scrapy.Field()
